790910115b7da735d3d08ca5de2f7abc28082205
Le Minh Quang <lmquang96@gmail.com>
2021-11-26 09:26:31 +0700

    install prettier

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,26 +1,26 @@
 // Graph
-var ctx = document.getElementById("myChart");
+var ctx = document.getElementById('myChart');
 
 var myChart = new Chart(ctx, {
-  type: "line",
+  type: 'line',
   data: {
     labels: [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
     ],
     datasets: [
       {
         data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
         lineTension: 0,
-        backgroundColor: "transparent",
-        borderColor: "#007bff",
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
         borderWidth: 4,
-        pointBackgroundColor: "#007bff",
+        pointBackgroundColor: '#007bff',
       },
     ],
   },
